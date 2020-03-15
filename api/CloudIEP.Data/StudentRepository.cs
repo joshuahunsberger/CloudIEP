85d5c0227469f92cab0d08bfbfb2697b6aed1367
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-14 23:27:39 -0400

    Add basic StudentRepository

---
+++ api/CloudIEP.Data/StudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using CloudIEP.Data.CosmosDB;
+using CloudIEP.Data.Models;
+
+namespace CloudIEP.Data
+{
+    public class StudentRepository : CosmosDbRepository<Student>, IRepository<Student>
+    {
+        public StudentRepository(ICosmosDbClientFactory factory) : base(factory) { }
+
+        public override string CollectionName => "students";
+    }
+}
