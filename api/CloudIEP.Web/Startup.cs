ec5270c93bb98fd019a6069be8090d3439f82bee
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-15 14:56:48 -0400

    Remove auto-generated comments

--- api/CloudIEP.Web/Startup.cs
+++ api/CloudIEP.Web/Startup.cs
@@ -21,7 +21,6 @@ namespace CloudIEP.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddData(Configuration);
@@ -41,7 +40,6 @@ namespace CloudIEP.Web
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
