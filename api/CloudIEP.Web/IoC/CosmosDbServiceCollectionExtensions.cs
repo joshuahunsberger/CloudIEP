0ca9a1e4a3f680dfead55918e1a604191e35aebd
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-15 11:05:50 -0400

    Add setup for CosmosDb in web project

---
+++ api/CloudIEP.Web/IoC/CosmosDbServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using CloudIEP.Data.CosmosDB;
+using CloudIEP.Web.Options;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CloudIEP.Web.IoC
+{
+    public static class CosmosDbServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services, CosmosDbOptions cosmosDbOptions)
+        {
+            var documentClient = new DocumentClient(cosmosDbOptions.Endpoint, cosmosDbOptions.Key, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+            documentClient.OpenAsync().Wait();
+
+            var collectionNames = cosmosDbOptions.Collections.Select(n => n.Name).ToList();
+
+            var cosmosDbClientFactory = new CosmosDbClientFactory(cosmosDbOptions.DatabaseName, collectionNames, documentClient);
+            cosmosDbClientFactory.EnsureDbSetupAsync().Wait();
+
+            services.AddSingleton<ICosmosDbClientFactory>(cosmosDbClientFactory);
+
+            return services;
+        }
+    }
+}
