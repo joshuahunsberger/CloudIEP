8fe99f4fbb503e5a9ba931e7b799a6ce194ad24c
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-14 20:01:06 -0400

    Add repository example code from Azure-Samples GitHub repo
    https://github.com/Azure-Samples/PartitionedRepository

---
+++ api/CloudIEP.Data/CosmosDB/CosmosDbClientFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace CloudIEP.Data.CosmosDB
+{
+    // Borrowed from Microsoft's Azure Samples repo for CosmosDB Repository pattern
+    //
+    // GitHub:
+    // https://github.com/Azure-Samples/PartitionedRepository/tree/master/TodoService.Infrastructure/Data
+
+    public interface ICosmosDbClientFactory
+    {
+        ICosmosDbClient GetClient(string collectionName);
+    }
+
+    public class CosmosDbClientFactory : ICosmosDbClientFactory
+    {
+        private readonly string _databaseName;
+        private readonly List<string> _collectionNames;
+        private readonly IDocumentClient _documentClient;
+
+        public CosmosDbClientFactory(string databaseName, List<string> collectionNames, IDocumentClient documentClient)
+        {
+            _databaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+            _collectionNames = collectionNames ?? throw new ArgumentNullException(nameof(collectionNames));
+            _documentClient = documentClient ?? throw new ArgumentNullException(nameof(documentClient));
+        }
+
+        public ICosmosDbClient GetClient(string collectionName)
+        {
+            if (!_collectionNames.Contains(collectionName))
+            {
+                throw new ArgumentException($"Unable to find collection: {collectionName}");
+            }
+
+            return new CosmosDbClient(_databaseName, collectionName, _documentClient);
+        }
+
+        public async Task EnsureDbSetupAsync()
+        {
+            await _documentClient.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_databaseName));
+
+            foreach (var collectionName in _collectionNames)
+            {
+                await _documentClient.ReadDocumentCollectionAsync(
+                    UriFactory.CreateDocumentCollectionUri(_databaseName, collectionName));
+            }
+        }
+    }
+}
