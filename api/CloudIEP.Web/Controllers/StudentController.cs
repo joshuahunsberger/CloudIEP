ef31635ce79e3a8dab6406f67ab5cb0389c279d0
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-15 12:07:55 -0400

    Add CRUD endpoint to manage students

---
+++ api/CloudIEP.Web/Controllers/StudentController.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using CloudIEP.Data;
+using CloudIEP.Data.Exceptions;
+using CloudIEP.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudIEP.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var studentResponse = await _studentRepository.AddAsync(student);
+            return Ok(studentResponse);
+        }
+
+        [HttpGet("{studentId}")]
+        public async Task<ActionResult<Student>> GetStudent(string studentId)
+        {
+            try
+            {
+                var student = await _studentRepository.GetByIdAsync(studentId);
+                return Ok(student);
+            }
+            catch (EntityNotFoundException)
+            {
+                return NotFound(studentId);
+            }
+        }
+
+        [HttpPut("{studentId}")]
+        public async Task<ActionResult<Student>> UpdateStudent(string studentId, Student student)
+        {
+            if (student.Id != studentId)
+            {
+                return BadRequest(student.Id);
+            }
+
+            try
+            {
+                if (studentId == null)
+                {
+                    return NotFound(studentId);
+                }
+
+                await _studentRepository.UpdateAsync(student);
+                return Ok();
+            }
+            catch (EntityNotFoundException)
+            {
+                return NotFound(studentId);
+            }
+        }
+
+        [HttpDelete("{studentId}")]
+        public async Task<ActionResult> DeleteStudent(string studentId)
+        {
+            try
+            {
+                var student = await _studentRepository.GetByIdAsync(studentId);
+
+                await _studentRepository.DeleteAsync(student);
+
+                return NoContent();
+            }
+            catch (EntityNotFoundException)
+            {
+                return NotFound(studentId);
+            }
+        }
+    }
+}
