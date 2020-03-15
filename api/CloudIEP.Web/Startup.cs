376c13cd53072fe8bc8ff0a03dee9559c5e5e8e3
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-15 11:05:50 -0400

    Add setup for CosmosDb in web project

--- api/CloudIEP.Web/Startup.cs
+++ api/CloudIEP.Web/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CloudIEP.Web.IoC;
+using CloudIEP.Web.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CloudIEP.Web
 {
@@ -27,6 +22,10 @@ namespace CloudIEP.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var cosmosDbOptions = Configuration.GetSection("CosmosDB")
+                .Get<CosmosDbOptions>();
+            services.AddCosmosDb(cosmosDbOptions);
+
             services.AddCors(options => options.AddPolicy(CorsPolicy, builder =>
             {
                 builder.WithOrigins("http://localhost:3000")
