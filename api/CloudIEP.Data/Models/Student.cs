6a0a5c6a283a0e7be28ca1f7eb0d89532c414529
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-14 23:27:39 -0400

    Add basic StudentRepository

---
+++ api/CloudIEP.Data/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CloudIEP.Data.Models
+{
+    public class Student : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
