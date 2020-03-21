cf9ae757228fc733abc82d5a314f949dbbd8ca7b
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-03-21 16:39:19 -0400

    Add material UI component package

--- cloud-iep-client/src/App.tsx
+++ cloud-iep-client/src/App.tsx
@@ -1,28 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import WeatherForecasts from './WeatherForecasts';
+
+import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <WeatherForecasts />
-      </div>
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Cloud IEP</Typography>
+          </Toolbar>
+        </AppBar>
+        <Typography variant="h1">Home</Typography>
+      </Box>
     </div>
   );
 }
