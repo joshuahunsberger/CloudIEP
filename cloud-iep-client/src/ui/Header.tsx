f1e4bef65fbffb4c3411b535fe253b87ada48183
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-04 19:35:57 -0400

    Add link to name in app bar

--- cloud-iep-client/src/ui/Header.tsx
+++ cloud-iep-client/src/ui/Header.tsx
@@ -1,26 +1,30 @@
-import { AppBar, Toolbar, Typography, makeStyles, useTheme } from '@material-ui/core';
+import { AppBar, makeStyles, Toolbar, Typography, useTheme } from '@material-ui/core';
 import { Cloud } from '@material-ui/icons';
 import React from "react";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     icon: {
         marginRight: theme.spacing(2),
     },
-    appName: {
+    homeLink: {
+        textDecoration: 'none',
+        color: "inherit",
         flex: 1
     }
 }));
 
 function Header() {
     const theme = useTheme();
-
     const classes = useStyles(theme);
 
     return (
         <AppBar position="static">
             <Toolbar >
                 <Cloud className={classes.icon} />
-                <Typography variant="h6" className={classes.appName}>Cloud IEP</Typography>
+                <Link to="/" className={classes.homeLink}>
+                    <Typography variant="h6">Cloud IEP</Typography>
+                </Link>
                 <Typography>Login</Typography>
             </Toolbar>
         </AppBar>
