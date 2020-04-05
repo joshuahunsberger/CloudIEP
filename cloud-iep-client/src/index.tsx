d1ab134da2246ff2526a925e25d39bc45401b2b2
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-04 23:13:25 -0400

    Add initial Auth0 login flow to React app

--- cloud-iep-client/src/index.tsx
+++ cloud-iep-client/src/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import { Auth0Provider } from "./react-auth0-spa";
 import * as serviceWorker from './serviceWorker';
+import history from "./utils/history";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = (result: RedirectLoginResult) => {
+    const appState = result?.appState;
+    history.push(
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    );
+};
+
+const options: Auth0ClientOptions = {
+    domain: process.env.REACT_APP_AUTH0_DOMAIN ?? "",
+    client_id: process.env.REACT_APP_AUTH0_CLIENTID ?? "",
+    redirect_uri: window.location.origin
+}
+
+ReactDOM.render(
+    <Auth0Provider
+        onRedirectCallback={onRedirectCallback}
+        initOptions={options}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ cloud-iep-client/src/react-auth0-spa.tsx
@@ -0,0 +1,110 @@
+import createAuth0Client from "@auth0/auth0-spa-js";
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
+import React, { useContext, useEffect, useState } from "react";
+
+const DEFAULT_REDIRECT_CALLBACK = (result: RedirectLoginResult) =>
+    window.history.replaceState({}, document.title, window.location.pathname);
+
+interface Auth0ContextState {
+    isAuthenticated: boolean;
+    user: any;
+    loading: boolean;
+    popupOpen: boolean;
+    loginWithPopup: (option: PopupLoginOptions, config: PopupConfigOptions) => Promise<void>;
+    handleRedirectCallback: () => Promise<RedirectLoginResult>;
+    getIdTokenClaims: (options?: getIdTokenClaimsOptions | undefined) => Promise<IdToken>;
+    loginWithRedirect: (options?: RedirectLoginOptions) => Promise<void>;
+    getTokenSilently: (options?: GetTokenSilentlyOptions) => Promise<string>;
+    getTokenWithPopup: (p: GetTokenWithPopupOptions) => Promise<string>;
+    logout: (options?: LogoutOptions) => void;
+}
+
+export const Auth0Context = React.createContext({} as Auth0ContextState);
+export const useAuth0 = () => useContext(Auth0Context);
+
+interface Auth0ProviderProps {
+    children: React.ReactNode;
+    onRedirectCallback: (result: RedirectLoginResult) => void;
+    initOptions: Auth0ClientOptions;
+}
+export const Auth0Provider = ({
+    children,
+    onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+    initOptions
+}: Auth0ProviderProps) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState();
+    const [auth0Client, setAuth0] = useState<Auth0Client>();
+    const [loading, setLoading] = useState(true);
+    const [popupOpen, setPopupOpen] = useState(false);
+
+    useEffect(() => {
+        const initAuth0 = async () => {
+            const auth0FromHook = await createAuth0Client(initOptions);
+            setAuth0(auth0FromHook);
+
+            if (window.location.search.includes("code=") &&
+                window.location.search.includes("state=")) {
+                const { appState } = await auth0FromHook.handleRedirectCallback();
+                onRedirectCallback(appState);
+            }
+
+            const isAuthenticated = await auth0FromHook.isAuthenticated();
+
+            setIsAuthenticated(isAuthenticated);
+
+            if (isAuthenticated) {
+                const user = await auth0FromHook.getUser();
+                setUser(user);
+            }
+
+            setLoading(false);
+        };
+        initAuth0();
+        // eslint-disable-next-line
+    }, []);
+
+    const loginWithPopup = async (params = {}) => {
+        setPopupOpen(true);
+        try {
+            await auth0Client!.loginWithPopup(params);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setPopupOpen(false);
+        }
+        const user = await auth0Client!.getUser();
+        setUser(user);
+        setIsAuthenticated(true);
+    };
+
+    const handleRedirectCallback = async () => {
+        setLoading(true);
+        var result = await auth0Client!.handleRedirectCallback();
+        const user = await auth0Client!.getUser();
+        setLoading(false);
+        setIsAuthenticated(true);
+        setUser(user);
+        return result;
+    };
+
+    return (
+        <Auth0Context.Provider
+            value={{
+                isAuthenticated,
+                user,
+                loading,
+                popupOpen,
+                loginWithPopup,
+                handleRedirectCallback,
+                getIdTokenClaims: (p) => auth0Client!.getIdTokenClaims(p),
+                loginWithRedirect: (p) => auth0Client!.loginWithRedirect(p),
+                getTokenSilently: (p) => auth0Client!.getTokenSilently(p),
+                getTokenWithPopup: (...p) => auth0Client!.getTokenWithPopup(...p),
+                logout: (p) => auth0Client!.logout(p)
+            }}
+        >
+            {children}
+        </Auth0Context.Provider>
+    );
+};
