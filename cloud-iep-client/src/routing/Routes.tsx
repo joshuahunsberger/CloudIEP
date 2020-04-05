4647d8a45433d107da5f1e8d9f680619ac4c8820
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-05 09:17:47 -0400

    Move Routes component to routing folder

--- cloud-iep-client/src/routing/Routes.tsx
+++ cloud-iep-client/src/routing/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './ui/Home';
-import StudentPage from './ui/student/StudentPage';
+import Home from '../ui/Home';
+import StudentPage from '../ui/student/StudentPage';
 
 const Routes = () => (
     <Switch>
--- cloud-iep-client/src/ui/Body.tsx
+++ cloud-iep-client/src/ui/Body.tsx
@@ -1,6 +1,6 @@
 import { Grid } from "@material-ui/core";
 import React from 'react';
-import Routes from '../Routes';
+import Routes from '../routing/Routes';
 
 function Body() {
     return (
