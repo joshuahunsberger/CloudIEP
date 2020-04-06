4bc411afeafcd873a0f820acc45c0687bb27279f
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-05 22:14:26 -0400

    Set up authorization in API project

---
+++ api/CloudIEP.Web/Authorization/Scopes.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudIEP.Web.Authorization
+{
+    public class Scopes
+    {
+        public const string OpenIdScope = "openid";
+        public const string ReadStudentsScope = "read:students";
+    }
+}
