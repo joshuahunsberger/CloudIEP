d0bde07d6742f808d10f068ff4d32cf0ff032f8b
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-06 22:48:53 -0400

    Add React error boundary for catching component errors

--- cloud-iep-client/src/ui/Body.tsx
+++ cloud-iep-client/src/ui/Body.tsx
@@ -1,13 +1,16 @@
 import { Grid } from "@material-ui/core";
 import React from 'react';
 import Routes from '../routing/Routes';
+import ErrorBoundary from "./ErrorBoundary";
 
 function Body() {
     return (
         <Grid container>
             <Grid item xs={false} sm={2} />
             <Grid item xs={12} sm={8} >
-                <Routes />
+                <ErrorBoundary logError={console.log}>
+                    <Routes />
+                </ErrorBoundary>
             </Grid>
             <Grid item xs={false} sm={2} />
         </Grid>
