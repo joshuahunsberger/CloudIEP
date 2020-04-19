a99125594d9cd3b436137597911fd73fac012d76
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-19 14:10:08 -0400

    Add delete confirmation

--- cloud-iep-client/src/ui/student/StudentTable.tsx
+++ cloud-iep-client/src/ui/student/StudentTable.tsx
@@ -1,8 +1,9 @@
 import { Card, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import { Delete, Edit } from "@material-ui/icons";
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useState } from 'react';
 import { Student } from '../../students/Student';
+import ConfirmDialog from '../ConfirmDialog';
 
 interface StudentTableProps {
     students?: ReadonlyArray<Student>
@@ -11,32 +12,57 @@ interface StudentTableProps {
 }
 
 const StudentTable: React.FC<StudentTableProps> = ({ students, setEditing, deleteStudent }: StudentTableProps) => {
+    const [open, setOpen] = useState(false);
+    const [selectedStudentId, setSelectedStudentId] = useState<string>();
+
+    const promptForDelete = (studentId: string) => {
+        setSelectedStudentId(studentId);
+        setOpen(true);
+    }
+
+    const confirmDelete = (shouldDelete: boolean) => {
+        if (shouldDelete && selectedStudentId) {
+            deleteStudent(selectedStudentId);
+        }
+        setOpen(false);
+    }
+
     return (
-        <TableContainer component={Card}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>First Name</TableCell>
-                        <TableCell>Last Name</TableCell>
-                        <TableCell>Date of Birth</TableCell>
-                        <TableCell />
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {students?.map(student => (
-                        <TableRow key={student.id}>
-                            <TableCell>{student.firstName}</TableCell>
-                            <TableCell>{student.lastName}</TableCell>
-                            <TableCell>{format(student.dateOfBirth, 'MM/dd/yyyy')}</TableCell>
-                            <TableCell>
-                                <IconButton onClick={() => setEditing(student.id)}><Edit /></IconButton>
-                                <IconButton onClick={() => deleteStudent(student.id)}><Delete /></IconButton>
-                            </TableCell>
+        <>
+            <ConfirmDialog
+                isOpen={open}
+                title="Delete Student"
+                content="Are you sure you want to delete this student?"
+                confirmButtonText="Delete"
+                declineButtonText="Cancel"
+                confirm={confirmDelete}
+            />
+            <TableContainer component={Card}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>First Name</TableCell>
+                            <TableCell>Last Name</TableCell>
+                            <TableCell>Date of Birth</TableCell>
+                            <TableCell />
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {students?.map(student => (
+                            <TableRow key={student.id}>
+                                <TableCell>{student.firstName}</TableCell>
+                                <TableCell>{student.lastName}</TableCell>
+                                <TableCell>{format(student.dateOfBirth, 'MM/dd/yyyy')}</TableCell>
+                                <TableCell>
+                                    <IconButton onClick={() => setEditing(student.id)}><Edit /></IconButton>
+                                    <IconButton onClick={() => promptForDelete(student.id)}><Delete /></IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     )
 }
 
