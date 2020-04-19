df1da9491b25f2a7e410ca64c6ea7d98aa035988
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-19 12:50:23 -0400

    Add basic student delete functionality

---
+++ cloud-iep-client/src/network/deleteRequest.ts
@@ -0,0 +1,20 @@
+import handleErrors from './handleErrors';
+
+async function deleteRequest(url: string, token?: string): Promise<void> {
+    const headers = new Headers();
+
+    if (token) {
+        headers.append("Authorization", `Bearer ${token}`);
+    }
+
+    const request = new Request(url, {
+        mode: 'cors',
+        headers: headers,
+        method: 'DELETE'
+    });
+
+    fetch(request)
+        .then(handleErrors);
+}
+
+export default deleteRequest;
