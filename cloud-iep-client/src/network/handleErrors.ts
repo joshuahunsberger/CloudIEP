ce9d73ea422699dc5f2b59b9d223d0159fd029fd
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-20 22:16:56 -0400

    Handle 404 error

--- cloud-iep-client/src/network/handleErrors.ts
+++ cloud-iep-client/src/network/handleErrors.ts
@@ -2,6 +2,8 @@ async function handleErrors(response: Response) {
   if (!response.ok) {
     if (response.status === 401) {
       throw new Error('Unauthorized');
+    } else if (response.status === 404) {
+      throw new Error('Not found.');
     }
     const errorText = await response.text();
     console.log('Error processing request: ' + errorText);
