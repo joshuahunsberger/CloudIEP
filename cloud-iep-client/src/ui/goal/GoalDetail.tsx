400cccbf40e3265da9f5b6763c624e54b67aca8c
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-26 18:26:08 -0400

    Add placeholders for goal detail

--- cloud-iep-client/src/ui/goal/GoalDetail.tsx
+++ cloud-iep-client/src/ui/goal/GoalDetail.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useGoalByUrl from '../../goals/useGoalByUrl';
+import ApiStatus from '../../types/ApiStatus';
+import { CircularProgress, Typography } from '@material-ui/core';
 
 const GoalDetail = () => {
+  const { id } = useParams();
+  const url = 'http://localhost:5000/api/Goal/' + id;
+  const service = useGoalByUrl(url);
+
   return (
     <>
-      <div>There should be some detail here.</div>
+      {service.status === ApiStatus.Loading && <CircularProgress />}
+      {service.status === ApiStatus.Loaded && (
+        <Typography>Loaded goal</Typography>
+        // TODO: Add data point
+        // TODO: Graph data points
+      )}
+      {service.status === ApiStatus.Error && (
+        <Typography variant="h6">
+          There was an error retrieving this goal. {service.error.message}
+        </Typography>
+      )}
     </>
   );
 };
