e83f8c301987a1aa6dd7ab046677286309a1dc6d
Joshua Hunsberger <jhunsberger3@gatech.edu>
2020-04-26 15:48:49 -0400

    Add a placeholder for goal details

---
+++ cloud-iep-client/src/ui/goal/GoalDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoalDetail = () => {
+  return (
+    <>
+      <div>There should be some detail here.</div>
+    </>
+  );
+};
+
+export const goalDetailRoute = '/goal/:id';
+export default GoalDetail;
