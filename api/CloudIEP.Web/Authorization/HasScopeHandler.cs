3d1468242790631e8cc6304c7ebf5f3234ea026f
Joshua Hunsberger <joshua@hunsberger.co>
2022-02-13 16:45:45 -0500

    Switch to file-scoped namespaces in Web project

--- api/CloudIEP.Web/Authorization/HasScopeHandler.cs
+++ api/CloudIEP.Web/Authorization/HasScopeHandler.cs
@@ -2,25 +2,24 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
-namespace CloudIEP.Web.Authorization
+namespace CloudIEP.Web.Authorization;
+
+public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
 {
-    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
     {
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
         {
-            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
-            {
-                return Task.CompletedTask;
-            }
-
-            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
-
-            if (scopes.Any(s => s == requirement.Scope))
-            {
-                context.Succeed(requirement);
-            }
-
             return Task.CompletedTask;
         }
+
+        var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+
+        if (scopes.Any(s => s == requirement.Scope))
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
     }
 }
