d2b56881d7070d3ae8602a67ca0cc0326a789e32
Joshua Hunsberger <joshua@hunsberger.co>
2022-04-11 23:07:20 -0400

    Update routing in support of React Router upgrade

--- cloud-iep-client/src/routing/Routes.tsx
+++ cloud-iep-client/src/routing/Routes.tsx
@@ -10,31 +10,67 @@ import StudentDetail, { studentDetailRoute } from '../ui/student/StudentDetail';
 import Auth, { authCallbackRoute } from '../ui/user/Auth';
 import Profile, { profileRoute } from '../ui/user/Profile';
 
-// Logged-in routing inspired by John Reilly on GitHub:
-// https://github.com/johnnyreilly/auth0-react-typescript-asp-net-core
-
-const privateRoutes = [
-  { path: profileRoute, component: Profile },
-  { path: studentsRoute, component: SimpleStudentPage },
-  { path: authCallbackRoute, component: Auth },
-  { path: studentDetailRoute, component: StudentDetail },
-  { path: goalDetailRoute, component: GoalDetail },
-];
-
 const Routes = () => {
-  const { isAuthenticated } = useAuth0();
-
   return (
     <Switch>
-      <Route path={homeRoute} exact={true} component={Home} />
-      {isAuthenticated
-        ? privateRoutes.map(({ path, component }) => (
-            <Route key={path} path={path} exact={true} component={component} />
-          ))
-        : null}
+      <Route path={homeRoute} exact={true}>
+        <Home />
+      </Route>
+      <Route
+        path={profileRoute}
+        render={() => (
+          <RequireAuth redirectTo={homeRoute}>
+            <Profile />
+          </RequireAuth>
+        )}
+      />
+      <Route
+        path={studentsRoute}
+        render={() => (
+          <RequireAuth redirectTo={homeRoute}>
+            <SimpleStudentPage />
+          </RequireAuth>
+        )}
+      />
+      <Route
+        path={authCallbackRoute}
+        render={() => (
+          <RequireAuth redirectTo={homeRoute}>
+            <Auth />
+          </RequireAuth>
+        )}
+      />
+      <Route
+        path={studentDetailRoute}
+        render={() => (
+          <RequireAuth redirectTo={homeRoute}>
+            <StudentDetail />
+          </RequireAuth>
+        )}
+      />
+      <Route
+        path={goalDetailRoute}
+        render={() => (
+          <RequireAuth redirectTo={homeRoute}>
+            <GoalDetail />
+          </RequireAuth>
+        )}
+      />
       <Redirect key="redirect" to={homeRoute} />
     </Switch>
   );
+};
+
+type PrivateRouteProps = {
+  redirectTo: string;
+  children: JSX.Element;
+};
+
+// Example taken from
+// https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f#get-started-upgrading-today
+const RequireAuth = ({ redirectTo, children }: PrivateRouteProps) => {
+  const { isAuthenticated } = useAuth0();
+  return isAuthenticated ? children : <Redirect to={redirectTo} />;
 };
 
 export default Routes;
