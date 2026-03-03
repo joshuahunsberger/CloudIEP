45d5682399dadd02de2744ff72a5b7216082aea6
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-02 20:13:57 -0500

    Update eslint config syntax to replace deprecation

--- cloud-iep-client/eslint.config.js
+++ cloud-iep-client/eslint.config.js
@@ -1,21 +1,19 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import prettier from 'eslint-config-prettier'
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
